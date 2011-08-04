e1233279a804e6b970cc2d4cc8f3b3fdaf090ce4
Sean McQuillan <mcquillan.sean@gmail.com>
2011-08-04 13:45:36 -0700

    Use ncurses, no one ever regrets doing that.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import setuptools
+
+setup(name='matrix',
+      version='0.1',
+      author='Sean McQuillan',
+      author_email='sean@twilio.com',
+      description='output filter that causes matrix spam',
+      scripts=['matrix'])
